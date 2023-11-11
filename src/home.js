6526b61f86a6b4bf40bdd0f0bc5de4938a6d19be
saranwright <127801812+saranwright@users.noreply.github.com>
2023-11-11 09:26:11 -0600

    testimonials section done

--- src/home.js
+++ src/home.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import Hero from './hero';
 import Specials from './specials';
+import Testimonials from './testimonials';
 
 
 function Home(){
@@ -21,6 +22,7 @@ function Home(){
         <div className="home">
             <Hero />
             <Specials />
+            <Testimonials />
             <div className="main">
             <Link to={{pathname: '/home', hash: '#test'}}>Test</Link>
             <p>One</p>
--- src/testimonials.js
+++ src/testimonials.js
@@ -1,9 +1,55 @@
 import './App.css';
+import Customer from './assets/circle-user-light.svg';
+import Star from './assets/star-light.svg';
+import HalfStar from './assets/star-half-light.svg';
 
 function Testimonials(){
     return(
         <div className="testimonials">
-        
+            <h2>Testimonials</h2>
+            <div className="ratings">
+                <div className="ratingBox">
+                    <p><img src={Star} alt="star" /><img src={Star} alt="star" /><img src={Star} alt="star" /><img src={HalfStar} alt="star" /></p>
+                    <div className="customer">
+                        <img src={Customer} alt="user icon"/>
+                        <p>Name</p>
+                    </div>
+                    <div className="review">
+                        “Review text.”
+                    </div>
+                </div>
+                <div className="ratingBox">
+                    <p><img src={Star} alt="star" /><img src={Star} alt="star" /><img src={Star} alt="star" /><img src={Star} alt="star" /></p>
+                        <div className="customer">
+                            <img src={Customer} alt="user icon"/>
+                            <p>Name</p>
+                        </div>
+                        <div className="review">
+                            “Review text.”
+                        </div>
+                </div>
+                <div className="ratingBox">
+                    <p><img src={Star} alt="star" /><img src={Star} alt="star" /><img src={Star} alt="star" /><img src={Star} alt="star" /><img src={Star} alt="star" /></p>
+                        <div className="customer">
+                            <img src={Customer} alt="user icon"/>
+                            <p>Name</p>
+                        </div>
+                        <div className="review">
+                            “Review text.”
+                        </div>
+                </div>
+                <div className="ratingBox">
+                    <p><img src={Star} alt="star" /><img src={Star} alt="star" /><img src={Star} alt="star" /><img src={Star} alt="star" /></p>
+                        <div className="customer">
+                            <img src={Customer} alt="user icon"/>
+                            <p>Name</p>
+                        </div>
+                        <div className="review">
+                            “Review text.”
+                        </div>
+                </div>
+
+            </div>
         </div>
     );
 }
