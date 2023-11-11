0ef27e2697059674aac05e5d7ae30ea62555388e
saranwright <127801812+saranwright@users.noreply.github.com>
2023-11-11 17:59:03 -0600

    minor changes

--- src/components/about.js
+++ src/components/about.js
@@ -13,7 +13,7 @@ function About(){
                 <p>Today, Little Lemon is a thriving restaurant that is known for its delicious food, friendly service, and warm atmosphere. Mario and Adrian are proud to be able to share their love of Mediterranean cuisine with the people of Chicago, and they are committed to providing their customers with an unforgettable dining experience.</p>
             </div>
             <div>
-                <img src={Owners} alt="Mario and Adrian, owners of Little Lemon" />
+                <img src={Owners} alt="Mario and Adrian, co-owners of Little Lemon" />
             </div>
         </div>
     );
